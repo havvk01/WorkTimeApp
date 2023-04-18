d3d56d6e6c5a72cf498bca8e86e80cd4a865e202
havvk01 <havvk01@gmail.com>
2023-04-18 12:53:59 +0300

    Initial Commit

---
+++ WorkTimeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkTimeApp
+//
+//  Created by Slava Havvk on 18.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
