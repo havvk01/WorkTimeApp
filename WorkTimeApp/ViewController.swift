cffb25a03eb380b47be03ea2e66cdb2e1af04746
havvk01 <90330806+havvk01@users.noreply.github.com>
2023-04-19 20:29:50 +0300

    -- add Button

--- WorkTimeApp/ViewController.swift
+++ WorkTimeApp/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import SnapKit
 
 final class ViewController: UIViewController {
+    private let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        
+        view.addSubview(button)
+        button.backgroundColor = .blue
+        
+        button.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.size.equalTo(100)
+        }
     }
 
 
